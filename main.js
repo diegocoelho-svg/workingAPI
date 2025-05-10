fd3053f3f8fe685a0e9063c0ea73d908824fc695
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-05-10 12:00:19 -0300

    Passando parametros na função

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-fetch("http://localhost:3333/products")
-.then((response) => response.json())
-.then((data) => {
+// Utilizando o fetch com o then.
+// fetch("http://localhost:3333/products")
+// .then((response) => response.json())
+// .then((data) => {
+//   console.log(data)
+// })
+// .finally(() => {
+//   console.log("Acabou")
+// })
+
+// Utilizando o fetch com o async await
+async function fetchProducts() {
+  const response = await fetch("http://localhost:3333/products")
+  const data = await response.json()
   console.log(data)
-})
+}
+
+async function fetchProductById(id) {
+  const response = await fetch(`http://localhost:3333/products/${id}`)
+  const data = await response.json()
+  console.log(data)
+}
+
+//fetchProducts()
+fetchProductById("1")
 
